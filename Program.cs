8a9bdbc310e2e01d7033850870f0c3ddc286f79f
habibmirsyad <habmirs@gmail.com>
2022-03-25 15:58:11 +0700

    implementasu design by contract

--- Program.cs
+++ Program.cs
@@ -10,9 +10,18 @@ namespace tpModul5_1302201138
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("===NORMAL_TEST===");
             SayaTubeVideo tube = new SayaTubeVideo("Tutorial Design By Contract - [Habib]");
-            tube.IncreasePlayCount(2);
+            tube.IncreasePlayCount(5);
             tube.printVideoDetails();
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("===ERROR_TEST===");
+            SayaTubeVideo err = new SayaTubeVideo("1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890");
+            err.IncreasePlayCount(10000001);
+            err.printVideoDetails();
+
         }
     }
 }
--- SayaTubeVideo.cs
+++ SayaTubeVideo.cs
@@ -10,24 +10,41 @@ namespace tpModul5_1302201138
     {
         int id, playCount;
         string title;
-        Random r = new Random();
         
         public SayaTubeVideo(string title)
         {
-            this.title = title;
             playCount = 0;
-            id = r.Next(3,5);
+            try
+            {
+                if (title.Length > 100) throw new Exception("Panjang title melewati batas!");
+                this.title = title;
+                Random r = new Random();
+                id = r.Next(1, 6);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                this.title = "-";
+            }
         }
 
         public void IncreasePlayCount(int playCount)
         {
-            this.playCount = this.playCount + playCount;
+            try
+            {
+                int c = checked(playCount + 2137483647);
+                this.playCount = this.playCount + playCount;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         public void printVideoDetails()
         {
-            Console.WriteLine("ID         : "+ id);
-            Console.WriteLine("Title      : "+ title);
+            Console.WriteLine("        ID : "+ id);
+            Console.WriteLine("     Title : "+ title);
             Console.WriteLine("Play Count : "+ playCount);
 
         }
